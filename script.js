829d69c09bbf0bc08655bb59f7754e5954f59ac0
MokshMalde <maldemoksh437@gmail.com>
2024-04-13 12:15:18 +0530

    new starting with new repo.

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#menu-icon").addEventListener("click", () => {
-    document.querySelector(".navbar").classList.toggle("#menu-icon");
-    if(document.querySelector(".navbar").classList.contains("#menu-icon")){
-        document.querySelector(".navbar").style.display = "inline";
-    } else {
-        document.querySelector(".navbar").style.display = "none";
-    }
-});
