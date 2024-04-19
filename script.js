6f7c4b1c85b4c134d629b71b987657344d53c2e2
MokshMalde <maldemoksh437@gmail.com>
2024-04-19 16:47:32 +0530

    new changes

---
+++ script.js
@@ -0,0 +1,16 @@
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnIcon = document.querySelector(".toggle-btn i");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+toggleBtn.onclick = function(){
+    dropdownMenu.classList.toggle("open");
+    const isOpen = dropdownMenu.classList.contains("open")
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
